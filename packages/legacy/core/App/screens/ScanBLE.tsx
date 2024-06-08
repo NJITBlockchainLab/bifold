4575fa16e1c8d06d475981c41855ba925481374c
Amish Faldu <amish.faldu@gmail.com>
2024-06-08 01:55:59 -0400

    chore: added bluetooth functionality for ios
    Signed-off-by: Amish Faldu <amish.faldu@gmail.com>

--- packages/legacy/core/App/screens/ScanBLE.tsx
+++ packages/legacy/core/App/screens/ScanBLE.tsx
@@ -18,6 +18,17 @@ const ScanBLE: React.FC<ScanProps> = ({ navigation, route }) => {
   const [showDisclosureModal, setShowDisclosureModal] = useState<boolean>(true)
   const [disclosureType, setDisclosureType] = useState<DisclosureTypes>('NearbyDevicesDisclosure')
   const { t } = useTranslation()
+  // const { agent } = useAgent()
+  // const record = useConnectionByOutOfBandId(recordId || '')
+  let receivedInvitation = ''
+  let implicitInvitations = false
+  if (route?.params && route.params['implicitInvitations']) {
+    implicitInvitations = route.params['implicitInvitations']
+  }
+  let reuseConnections = false
+  if (route?.params && route.params['reuseConnections']) {
+    reuseConnections = route.params['reuseConnections']
+  }
 
   const permissionFlow = async (
     method: MultiplePermissionContract,
@@ -46,6 +57,18 @@ const ScanBLE: React.FC<ScanProps> = ({ navigation, route }) => {
 
     return false
   }
+
+  // const handleRead = ({ value }: { value: string }) => {
+  //   console.log('Received data from', cuuid, 'in service', uuid)
+  //   console.log('Data:', value)
+
+  //   receivedInvitation += value
+
+  //   if (value.includes('\n')) {
+  //     receivedInvitation.replace('\n', '')
+  //     handleInvitation(receivedInvitation)
+  //   }
+  // }
 
   useEffect(() => {
     const asyncEffect = async () => {
